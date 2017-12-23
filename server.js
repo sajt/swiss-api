da299baaf4ae630fcd0e1b65b868e432cc283bd1
Tamás Ámon <tamas.amon@gmail.com>
2017-12-23 16:06:19 +0100

    Allow CORS (I hope)

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ var express = require('express'),
     port = process.env.PORT || 3000,
     bodyParser = require('body-parser');
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
@@ -14,6 +10,10 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 
 /*app.use(function(req, res, next) {
